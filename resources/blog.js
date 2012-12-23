836e8e6dde11c724137b56c9d084857b72582159
leonchen <leon@factual.com>
2012-12-23 18:58:57 +0800

    implement simple blog adding

--- resources/blog.js
+++ resources/blog.js
@@ -1,42 +1,58 @@
+var config = require('../config');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(config.site.name.replace(" ", "_")+".blogDB");
+var postSecret = config.site.resources.blog.postSecret;
+
 var Blog = function () {
+  this.initDB();
 };
 
+
 Blog.prototype = {
+  initDB: function () {
+    db.run("CREATE TABLE IF NOT EXISTS blog_topics (timestamp INTEGER, title TEXT, content TEXT)");
+  },
+
   handle: function (app) {
+    var self = this;
+    app.all("/post", function (req, res) {
+      if (req.body && req.body.secret == postSecret) {
+        self.addTopic(req.body, function () {
+          res.redirect("/?source=blog");
+        });
+      } else {
+        res.render('post');
+      }
+    });
+  },
+
+  addTopic: function (params, cb) {
+    var title = params.title || 'No Subject';
+    var content = params.content || 'No Content';
+    db.run("insert into blog_topics(timestamp, title, content) values(?, ?, ?)", [Date.now(), title, content], function (err) { cb(); });
   },
 
   getTopics: function (req, res, cb) {
-    this.getMoreTopics(req, res, cb);
+    if ("max_id" in req.topicParams) {
+      this.getMoreTopics(req, res, cb);
+    } else {
+      this.getLatestTopics(req, res, cb);
+    }
   },
 
   getMoreTopics: function (req, res, cb) {
-    var timestamp = parseInt(req.query.timestamp) || (new Date()).getTime();
-    var per = parseInt(req.query.per) || 20;
-    var topics = [];
-
-    for (var i=0;i<5;i++){
-      topics.push({
-        timestamp: timestamp - 2000,
-        title: 'blog@'+timestamp,
-        content: 'blog content'
-      });
-    }
-
-    cb(null, topics); 
+    var timestamp = parseInt(req.topicParams.max_id) || Date.now();
+    var per = parseInt(req.topicParams.count) || 20;
+    db.all("select * from blog_topics where timestamp <= ? limit ?", [timestamp, per], function (err, rows) { 
+      cb(err, rows); 
+    });
   },
 
   getLatestTopics: function (req, res, cb) {
-    var timestamp = parseInt(req.query.timestamp);
-    var topics = [];
-    for (var i=0;i<10;i++){
-      topics.push({
-        timestamp: timestamp + 20000,
-        title: 'blog@'+timestamp,
-        content: 'blog content'
-      });
-    }
-
-    cb(null, topics); 
+    var timestamp = parseInt(req.topicParams.since_id);
+    db.all("select * from blog_topics where timestamp > ?", [timestamp], function (err, rows) { 
+      cb(err, rows); 
+    });
   }
 
 };
